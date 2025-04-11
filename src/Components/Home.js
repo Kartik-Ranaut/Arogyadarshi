dd963bf0ddca2aa56be81e9c5c056f2053ac10b9
Aseem Jain <jainaseem2004@gmail.com>
2025-04-11 12:33:28 +0530

    Fixed home section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <h1 className="home-title">Welcome to Arogyadarshi</h1>
+      <p className="home-subtitle">
+        An intelliget ML health assistant for predicting Heart Disease and
+        Diabetes
+      </p>
+
+      <div className="feature-cards">
+        <div className="card">
+          <h2>Heart Disease</h2>
+          <p>Input your medical details and get an instant prediction</p>
+          <button
+            onClick={() =>
+              navigate("/dashboard", { state: { selection: "heart" } })
+            }
+          >
+            Predict
+          </button>
+        </div>
+
+        <div className="card">
+          <h2> Diabetes</h2>
+          <p>Input your medical details and get an instant prediction</p>
+          <button
+            onClick={() =>
+              navigate("/dashboard", { state: { selection: "diabetes" } })
+            }
+          >
+            Predict
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
