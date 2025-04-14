8e8ef993d75ef93ae74db0ac037e1dbb662557a5
Kartik <kartikranaut@Kartiks-MacBook-Air.local>
2025-04-14 15:38:32 +0530

    implemented login and signup frontend

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import './login.css'
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
   return (
-    <div>
-    Login
+      <div class="signup-container">
+      <h2>Create Account</h2>
+      <form className="loginForm">
+        <div class="input-group">
+          <label for="name">Full Name</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div class="input-group">
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div class="input-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit" class="submitbtn">Login</button>
+        <p>not registered? click to <a href='/signup'>signup </a></p>
+      </form>
     </div>
   )
 }
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,7 +1,45 @@
 import React from 'react'
-
+import './signup.css'
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div class="signup-container">
+    <h2>Signup Form</h2>
+    <form className="">
+    <div className='signupForm'>
+    <div class="input-group">
+        <label for="name">Full Name</label>
+        <input type="text" id="name" required />
+      </div>
+      <div class="input-group">
+        <label for="age">Age</label>
+        <input type="number" id="age" required />
+      </div>
+      <div class="input-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" required />
+      </div>
+      <div class="input-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" required />
+      </div>
+      <div class="input-group">
+        <label for="phone">Phone Number</label>
+        <input type="tel" id="phone" required />
+      </div>
+      <div class="input-group">
+        <label for="disease">Disease (if any)</label>
+        <input type="text" id="disease" />
+      </div>
+    </div>
+      
+      <div class="input-group">
+        <label for="address">Address</label>
+        <textarea id="address" rows="2" required></textarea>
+      </div>
+      <button type="submit" class="submitbtn">Register</button>
+    <p>Already have an account? <a href='/login'>Login</a></p>
+    
+    </form>
+  </div>
   )
 }
