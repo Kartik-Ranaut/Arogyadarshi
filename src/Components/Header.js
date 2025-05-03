ee12cdf2fc2c4c95c8a863a663bb9ea9e140595b
Kartik-Ranaut <ranautk6@gmail.com>
2025-05-03 18:21:00 +0530

    user section reverted

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,24 @@
 import './header.css'
-import React from 'react'
+import React,{useState} from 'react'
 import { Link, Links, useNavigate , NavLink} from 'react-router-dom'
 
 export default function Header(props) {
+
+  const [isOpen, setIsOpen] = useState(false);
+   
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+ 
+
+  const handleLogout = () => {
+    alert('Logged out!');
+    // Clear token or do logout logic
+    document.cookie="token=;expires=Thu, 01 Jan 1970 00:00:00 UTC;"; props.setlogedin(false)
+    setIsOpen(false);
+  };
     const navigate=useNavigate();
     const btnCall =()=>{
         navigate('/login');
@@ -33,8 +49,35 @@ export default function Header(props) {
 
      
       {
-
-      props.islogedin ? <div>{props.user.name}</div>:<button className="loginbtn" onClick={btnCall}>
+        props.islogedin ? <div>
+       
+       <button className="user-name-button" onClick={toggleDropdown}>
+         {props.user.name} {isOpen ? '▲' : '▼'}
+       </button>
+ 
+       {isOpen && (
+         <div className="dropdown-menu">
+           <div className="user-info">
+             <h4>{props.user.name}</h4>
+             <p>{props.user.email}</p>
+             <p>{props.user.phone}</p>
+             <button className="manage-account-btn">Manage Account</button>
+           </div>
+           <hr />
+           <div className="menu-links">
+             <p onClick={()=>{navigate("/dashboard")}}>Dashboard</p>
+             <p onClick={()=>{navigate("/progress")}}>My Reports</p>
+           </div>
+           <hr />
+           <button className="logout-button" onClick={handleLogout}>
+             Log out
+           </button>
+         </div>
+       )}
+       
+       
+       
+       </div>:<button className="loginbtn" onClick={btnCall}>
         Login/Signup
       </button>
       }
